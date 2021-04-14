787b5b724372ae7b704ea13db37c8fa00efd2faa
adriana <picadoadriana7@gmail.com>
2021-04-14 19:58:53 -0400

    update index action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,17 @@
 class AppointmentsController < ApplicationController
 
     def index
+      if params[:customer_id].to_i == session[:user_id]
+
+         @customer = Customer.find_by_id(params[:customer_id])
+         @appointments = @customer.appointments
+
+      elsif current_customer
+         redirect_to customer_path(current_customer)
+
+      else
+         redirect_to '/' 
+      end
     end
 
     def new
