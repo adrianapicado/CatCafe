24a3f795037c8f6b74d5eb32fc022217bcb95639
adriana <picadoadriana7@gmail.com>
2021-04-16 17:08:37 -0400

    clean up

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -15,14 +15,14 @@ class CustomersController < ApplicationController
     end
     
     def show
-      if params[:id].to_i == session[:user_id]    
+     if params[:id].to_i == session[:user_id]    
         @customer = Customer.find_by_id(params[:id])
         @customer_appointments = @customer.appointments
-       elsif current_user
-             redirect_to customer_path(current_user)
-       else
+      elsif current_user
+            redirect_to customer_path(current_user)
+      else
             redirect_to '/' 
-        end
+       end
     end
     
     
@@ -30,7 +30,7 @@ class CustomersController < ApplicationController
      @customer = Customer.find(params[:id])
      @customer.update(customer_params)
      redirect_to customer_path(@customer)
-    end
+   end
     
     private 
 
